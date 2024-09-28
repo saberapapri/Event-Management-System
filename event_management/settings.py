0b2018fc4b9dcde02941cb532a6b1e4667b7879c
Papri <saberaakterpapri08@gmail.com>
2024-09-29 00:20:23 +0600

    initial commit
    Signed-off-by: Papri <saberaakterpapri08@gmail.com>

--- event_management/settings.py
+++ event_management/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,12 +117,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+#STATIC_URL = '/static/'
+
+#STATICFILES_DIRS = [
+   # BASE_DIR / "static",
+#]
+
+
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 
+# Static files directories
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'static'),  # Update this line
 ]
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
